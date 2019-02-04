6536dcacb3e559f235311bffa7bea5c42c2c49e8
Gleb Zavadskiy <zavadskiy@usf.edu>
2019-02-04 16:07:08 -0500

    Problem 3 is done by Gleb

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,17 +1,389 @@
 ﻿using System;
 
-namespace Assignment2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] a = new int[] { 1, 2, 3, 4, 5 };
-            int d = 2;
-            rotLeft(a, d);
+using System.Collections.Generic;
+using System.Linq;
 
-            Console.ReadKey();
+namespace Assignment2_S19
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            // left rotation
+
+            Console.WriteLine("Left Rotation");
+
+            int d = 4;
+
+            int[] a = { 1, 2, 3, 4, 5 };
+
+            int[] r = rotLeft(a, d);
+
+            displayArray(r);
+
+            Console.ReadLine();
+
+
+
+            // Maximum toys
+
+            Console.WriteLine("\n\nMaximum toys");
+
+            int k = 50;
+
+            int[] prices = { 1, 12, 5, 111, 200, 1000, 10 };
+
+            Console.WriteLine(maximumToys(prices, k));
+
+            Console.ReadLine();
+
+
+
+            // Balanced sums
+
+            Console.WriteLine("\n\nBalanced sums");
+
+            List<int> arr = new List<int> { 1,2,3 };
+
+            Console.WriteLine(balancedSums(arr));
+
+            Console.ReadLine();
+
+            // Missing numbers
+
+            Console.WriteLine("\n\nMissing numbers");
+
+            int[] arr1 = { 203, 204, 205, 206, 207, 208, 203, 204, 205, 206 };
+
+            int[] brr = { 203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204 };
+
+            int[] r2 = missingNumbers(arr1, brr);
+
+            displayArray(r2);
+
+
+
+            // grading students
+
+            Console.WriteLine("\n\nGrading students");
+
+            int[] grades = { 73, 67, 38, 33 };
+
+            int[] r3 = gradingStudents(grades);
+
+            displayArray(r3);
+
+
+
+            // find the median
+
+            Console.WriteLine("\n\nFind the median");
+
+            int[] arr2 = { 0, 1, 2, 4, 6, 5, 3 };
+
+            Console.WriteLine(findMedian(arr2));
+
+
+
+            // closest numbers
+
+            Console.WriteLine("\n\nClosest numbers");
+
+            int[] arr3 = { 5, 4, 3, 2 };
+
+            int[] r4 = closestNumbers(arr3);
+
+            displayArray(r4);
+
+
+
+            // Day of programmer
+
+            Console.WriteLine("\n\nDay of Programmer");
+
+            int year = 2017;
+
+            Console.WriteLine(dayOfProgrammer(year));
+
         }
 
+
+
+        static void displayArray(int[] arr)
+
+        {
+
+            Console.WriteLine();
+
+            foreach (int n in arr)
+
+            {
+
+                Console.Write(n + " ");
+
+            }
+
+        }
+
+
+
+        // Complete the rotLeft function below.
+
+        static int[] rotLeft(int[] a, int d)
+
+        {
+
+
+
+
+
+            while (d != 0)
+
+            {
+
+                int Subs;
+
+                Subs = a[0]; // to put the first element of the array in an auxiliary container to be moved
+
+
+
+                for (int i = 0; i < a.Length - 1; i++) //moves all elements one unit to the left
+
+                {
+
+                    a[i] = a[i + 1];
+
+
+
+                }
+
+                a[a.Length - 1] = Subs; //puts the first element at the end
+
+                d = d - 1; // controls the total required shifts
+
+
+
+
+
+            }
+
+            return a;
+
+        }
+
+
+
+        // Complete the maximumToys function below.
+
+        static int maximumToys(int[] prices, int k)
+
+        {
+
+            int[] SortedPrices = BubbleSort(prices);
+
+
+
+            int TotalPrice = 0;
+
+            int Counter = 0;
+
+
+
+            while (TotalPrice <= k)
+
+            {
+
+
+
+                TotalPrice = TotalPrice + SortedPrices[Counter];
+
+                Counter = Counter + 1;
+
+            }
+
+
+
+
+
+
+
+
+
+            return Counter - 1;
+
+        }
+
+
+
+        // Complete the balancedSums function below.
+
+        static string balancedSums(List<int> arr)
+
+        {
+            int[] array = arr.ToArray();
+            int length = array.Length;
+
+            if (length==1)
+            {
+                return "Yes";
+            }
+            else if (length == 2)
+            {
+                if (array[0]==0 | array[1]==0)
+                {
+                    return "Yes";
+                }
+            }
+            else
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    if (i == length - 1)
+                    {
+                        int sumLeft = new ArraySegment<int>(array, 0, i).Sum();
+                        int sumRight = 0;
+                        if (sumLeft == sumRight)
+                        {
+                            return "Yes";
+                        }
+                    }
+                    else if (i == 0)
+                    {
+                        int sumLeft = 0;
+                        int sumRight = new ArraySegment<int>(array, i + 1, length - i - 1).Sum();
+                        if (sumLeft == sumRight)
+                        {
+                            return "Yes";
+                        }
+                    }
+                    else
+                    {
+                        int sumLeft = new ArraySegment<int>(array, 0, i).Sum();
+                        int sumRight = new ArraySegment<int>(array, i + 1, length-i-1).Sum();
+                        if (sumLeft == sumRight)
+                        {
+                            return "Yes";
+                        }
+                    }
+                }
+            }
+            return "No";
+        }
+
+
+
+        // Complete the missingNumbers function below.
+
+        static int[] missingNumbers(int[] arr, int[] brr)
+
+        {
+
+            return new int[] { };
+
+        }
+
+
+
+
+
+        // Complete the gradingStudents function below.
+
+        static int[] gradingStudents(int[] grades)
+
+        {
+
+            return new int[] { };
+
+        }
+
+
+
+        // Complete the findMedian function below.
+
+        static int findMedian(int[] arr)
+
+        {
+
+            return 0;
+
+        }
+
+
+
+        // Complete the closestNumbers function below.
+
+        static int[] closestNumbers(int[] arr)
+
+        {
+
+            return new int[] { };
+
+        }
+
+
+
+        // Complete the dayOfProgrammer function below.
+
+        static string dayOfProgrammer(int year)
+
+        {
+
+            return "";
+
+        }
+
+
+
+        //Sort Method
+
+        static int[] BubbleSort(int[] arr) //sorts the array using bubble sort method
+
+        {
+
+            int Subs;
+
+            for (int i = 0; i < arr.Length; i++)
+
+            {
+
+                for (int j = 0; j < arr.Length - 1; j++)
+
+                {
+
+                    if (arr[j] > arr[j + 1])
+
+                    {
+
+                        Subs = arr[j + 1];
+
+                        arr[j + 1] = arr[j];
+
+                        arr[j] = Subs;
+
+                    }
+
+                }
+
+            }
+
+            return arr;
+
+
+
+        }
+
+
+
+
+
+
+
     }
+
 }
